fb93fa77c5c29c17afc23c69b8c644248b0e2aed
Claquettes <math@claq.fr>
2023-09-01 00:05:24 +0200

    removed react

--- src/pages/About.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About: React.FC = () => {
-  return <div>This is the About page.</div>;
-};
-
-export default About;
--- src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home: React.FC = () => {
-  return <div>This is the Home page.</div>;
-};
-
-export default Home;
